ed4a3217f1ef76473637429aadeb2d75f316088c
chenxueyou <chenxy@putao.com>
2018-06-27 11:44:21 +0800

    Add permissions check

--- source/app/system/repository/UserRepository.d
+++ source/app/system/repository/UserRepository.d
@@ -4,11 +4,16 @@ import entity.repository;
 
 import app.system.model.User;
 
-import entity.EntityManager;
+// import entity.EntityManager;
+import hunt;
 
 class UserRepository : EntityRepository!(User, int)
 {
     this(EntityManager manager = null) {
         super(manager);
+    }
+    this()
+    {
+        super(Application.getInstance().getEntityManagerFactory().createEntityManager());
     }
 }
