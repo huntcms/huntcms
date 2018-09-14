91f8b0399cb1056c9126188f25ca21762ab56881
zoujiaqing <zoujiaqing@gmail.com>
2018-09-14 19:05:13 +0800

    Add settings

---
+++ source/app/component/system/helper/SettingUtil.d
@@ -0,0 +1,51 @@
+module app.component.system.helper.SettingUtil;
+
+import app.component.system.helper.SettingObject;
+import app.component.system.repository.SettingRepository;
+
+class SettingUtil
+{
+    T settingInit(T)(Setting[string] settings)
+    {
+        auto so = new T;
+
+        mixin (generateItemsCode!T);
+
+        return so;
+    }
+
+    string generateItemsCode(T)()
+    {
+        string code = "Setting s;";
+
+        static foreach (t; __traits(derivedMembers, T))
+        {
+            static if (hasUDA!(t, SettingItem))
+            {
+                string key = getUDAs!(t, SettingItem)[0].key;
+                code ~= "s" ~ t ~ " = settings.get(" ~ key ~ ", "");\n";
+                code ~= "if (s !is null) so." ~ t ~ " = s.value;\n";
+            }
+        }
+
+        return code;
+    }
+}
+
+SettingObject setting()
+{
+    auto settingUtil = new SettingUtil;
+    if (_huntcmsSetting is null)
+    {
+        Setting[string] settings;
+        foreach (s; (new SettingRepository).findAll())
+        {
+            settings[s.key] = s;
+        }
+        _huntcmsSetting = settingUtil.settingInit!SettingObject(settings);
+    }
+
+    return _huntcmsSetting;
+}
+
+private SettingUtil _huntcmsSetting;
---
+++ source/app/component/system/repository/SettingRepository.d
@@ -0,0 +1,9 @@
+module app.component.system.repository.SettingRepository;
+
+import app.component.system.model.Setting;
+
+import hunt.entity;
+
+class SettingRepository : EntityRepository!(Setting, string)
+{
+}
